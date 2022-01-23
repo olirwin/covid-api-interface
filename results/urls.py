6f9432dd590580b118fc0142ad6f81f268887df3
Oliver Irwin <oliver.irwin.etu@univ-lille.fr>
2022-01-23 16:21:36 +0100

    feat : add view for whole data

--- results/urls.py
+++ results/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, FetchDataView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name = "index"),
+    path('data', FetchDataView.as_view(), name = "data"),
 ]
