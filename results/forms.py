6f1968d5385afa3d6aa9601ae45c35e8f56ae388
Oliver Irwin <oliver.irwin.etu@univ-lille.fr>
2022-01-23 16:21:20 +0100

    feat : update form

--- results/forms.py
+++ results/forms.py
@@ -5,9 +5,12 @@ from .models import Region
 
 class ChooseParamsDataForm(forms.Form) :
     choices = [(region.id, region) for region in Region.objects.all()]
-    region = forms.ChoiceField(label = "", widget = forms.Select(
+    region = forms.ChoiceField(label = "Region", widget = forms.Select(
             attrs = {"class" : "form-select", "aria-label" : "Choix de la région"}),
                                 choices = choices, required = True)
-    start_date = forms.DateField(label = "Du", widget = AdminDateWidget(),
+    start_date = forms.DateField(label = "Du", widget = AdminDateWidget(
+        attrs = {"class" : "form-control", "aria-label" : "Date de début"}),
                                  required = True)
-    end_date = forms.DateField(label = "Au", widget = AdminDateWidget(), required = True)
+    end_date = forms.DateField(label = "Au", widget = AdminDateWidget(
+        attrs = {"class" : "form-control", "aria-label" : "Date de début"}),
+                               required = True)
