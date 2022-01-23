4cdb9d9cb2c3826cfe983d2f3b38ae5e77172613
Oliver Irwin <oliver.irwin.etu@univ-lille.fr>
2022-01-23 17:38:20 +0100

    fix : uncomment form

--- results/forms.py
+++ results/forms.py
@@ -4,15 +4,13 @@ from .models import Region
 
 
 class ChooseParamsDataForm(forms.Form) :
-    # choices = [(region.id, region) for region in Region.objects.all()]
-    # region = forms.ChoiceField(label = "Region", widget = forms.Select(
-    #         attrs = {"class" : "form-select", "aria-label" : "Choix de la région"}),
-    #                             choices = choices, required = True)
-    # start_date = forms.DateField(label = "Du", widget = AdminDateWidget(
-    #     attrs = {"class" : "form-control", "aria-label" : "Date de début"}),
-    #                              required = True)
-    # end_date = forms.DateField(label = "Au", widget = AdminDateWidget(
-    #     attrs = {"class" : "form-control", "aria-label" : "Date de début"}),
-    #                            required = True)
-
-    pass
+    choices = [(region.id, region) for region in Region.objects.all()]
+    region = forms.ChoiceField(label = "Region", widget = forms.Select(
+            attrs = {"class" : "form-select", "aria-label" : "Choix de la région"}),
+                                choices = choices, required = True)
+    start_date = forms.DateField(label = "Du", widget = AdminDateWidget(
+        attrs = {"class" : "form-control", "aria-label" : "Date de début"}),
+                                 required = True)
+    end_date = forms.DateField(label = "Au", widget = AdminDateWidget(
+        attrs = {"class" : "form-control", "aria-label" : "Date de début"}),
+                               required = True)
